5309d82a2e2a18c7a39f457035d718ad4b0042f6
Abdullahtanzeem <abdulltanzz313@gmail.com>
2025-10-30 10:46:44 +0300

    Basic Arithmatic operators in JS

---
+++ index.js
@@ -0,0 +1,17 @@
+let sum = 5+3;
+console.log("Sum: ", sum);
+
+let difference = 10-2;
+console.log("Difference: ", difference);
+
+let product = 4*2;
+console.log("Product:", product);
+
+let quotient = 20 / 5;
+console.log("Quotient:", quotient);
+
+let remainder = 28 % 10;
+console.log("Remainder:", remainder);
+
+let power= 2 ** 3;
+console.log("Power:", power);
